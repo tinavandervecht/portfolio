322218609a5dc73181bb0c5402ba115139372f14
Tina Vandervecht <tina.vandervecht@roomroster.com>
2017-09-03 18:12:55 -0400

    Adding form functionality

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,8 +2,10 @@ window.$ = window.jQuery = require('jquery');
 
 var particles = require('./components/background/particles.js');
 var modal = require('./components/modal.js');
+var validation = require('./components/validation.js');
 
 $(document).ready(function () {
     particles.init();
     modal.init();
+    validation.init();
 });
---
+++ resources/assets/js/components/validation.js
@@ -0,0 +1,55 @@
+'use strict';
+
+exports.init = init;
+
+function init() {
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+
+        $('.input', this).removeClass('error-invalid');
+        $('.input', this).removeClass('error-empty');
+
+        var firstName = $('#first-name', this);
+        var lastName = $('#last-name', this);
+        var email = $('#email', this);
+        var website = $('#website', this);
+        var message = $('#message', this);
+
+        var noFirstName = !$('input[name="first_name"]', firstName).val();
+        var noLastName = !$('input[name="last_name"]', lastName).val();
+        var noEmail = !$('input[name="email"]', email).val();
+        var noMessage = !$('textarea[name="message"]', message).val();
+        var noWebsite = !$('input[name="website"]', website).val();
+
+        var invalidEmail = !validateEmail($('input[name="email"]', email).val());
+
+        if (noFirstName) {
+            firstName.addClass('error-empty');
+        }
+
+        if (noLastName) {
+            lastName.addClass('error-empty');
+        }
+
+        if (noEmail) {
+            email.addClass('error-empty');
+        } else if (invalidEmail) {
+            email.addClass('error-invalid');
+        }
+
+        if (noMessage) {
+            message.addClass('error-empty');
+        }
+
+        if (noFirstName || noLastName || noEmail || noMessage || !noWebsite || invalidEmail) {
+            return false;
+        } else {
+            $(this).unbind().submit();
+        }
+    })
+}
+
+function validateEmail($email) {
+    var emailReg = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    return emailReg.test( $email );
+}
