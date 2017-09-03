4b17ccc190754496509af650e496f8dee08afa92
Tina Vandervecht <tina.vandervecht@roomroster.com>
2017-09-03 18:12:22 -0400

    Styling out desktop work section with animated modal

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,7 +1,9 @@
 window.$ = window.jQuery = require('jquery');
 
 var particles = require('./components/background/particles.js');
+var modal = require('./components/modal.js');
 
 $(document).ready(function () {
     particles.init();
+    modal.init();
 });
