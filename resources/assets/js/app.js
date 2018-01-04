2f53a67c58ec754261d76348a898310f9b81b6cb
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-01-04 00:04:19 -0500

    Working on re-theming (and cleaning out old code)

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,20 +1,14 @@
 window.$ = window.jQuery = require('jquery');
 
 var particles = require('./components/background/particles.js');
-var hamburgers = require('./components/hamburgers.js');
 var validation = require('./components/validation.js');
-var navigation = require('./components/navigation.js');
 var easterEgg = require('./components/egg/zelda-easter-egg.js');
 var toastr = require('toastr');
-var graphs = require('./components/graphs.js');
 
 $(document).ready(function () {
     particles.init();
-    hamburgers.init();
     validation.init();
-    navigation.init();
     easterEgg.init();
-    graphs.init();
 
     if (typeof successfulSubmission != 'undefined' && successfulSubmission) {
         toastr.options.closeButton = true;
--- resources/views/index.php
+++ resources/views/index.php
@@ -41,83 +41,138 @@
     </div>
 
     <div id="main">
-        <?php include("./partials/navigation.php"); ?>
         <header class="header wrapper">
-            <h1>Tina Vandervecht.</h1>
-            <p>Developer. Passionate about trivia. Chronic talker.</p>
-            <a href="mailto:tvandervecht@gmail.com" class="available-flag">Available for freelance.</a>
-            <img src="/images/arrow-down.svg" alt="Scroll Down" class="scroll-down-img" />
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1>Tina Vandervecht.</h1>
+                        <p>Developer. Passionate about trivia. Chronic talker.</p>
+                        <a href="mailto:tvandervecht@gmail.com" class="available-flag">Available for freelance.</a>
+                        <img src="/images/arrow-down.svg" alt="Scroll Down" class="scroll-down-img" />
+                    </div>
+                </div>
+            </div>
         </header>
 
-        <section class="bg-white aboutSection" id="aboutSection">
-            <div class="wrapper">
-                <h2><span class="number text-secondary">01/</span> About.</h2>
-                <div class="avatar">
-                    <img src="/images/avatar.png" alt="Picture of Tina Vandervecht" />
+        <section id="aboutSection">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="/images/avatar.png" alt="Picture of Tina Vandervecht" style="max-width:100%"/>
+                    </div>
+                    <div class="col-md-8">
+                        <h1 class="card-title">About</h1>
+                        <div class="card">
+                            <h2>I'm a full stack developer with a passion for front end from London, Ontario.</h2>
+                            <p>
+                                this is where a super generic paragraph about me will go. Muffin jelly cheesecake lollipop cheesecake chocolate bar pastry marzipan. Marzipan halvah cotton candy. Toffee jujubes cupcake donut oat cake biscuit dessert jelly biscuit. Tart sweet roll powder pastry cake ice cream cupcake. Sweet lollipop bonbon chocolate powder apple pie. Jelly beans tart chocolate gingerbread topping jelly. Liquorice dragée oat cake tart candy canes pudding danish marzipan cheesecake. Danish sugar plum cheesecake apple pie pie marshmallow biscuit. Sugar plum icing jujubes gummi bears macaroon lollipop dragée.
+                            </p>
+                            <!-- <p>I’m Tina, a full-stack developer with a passion for all things front-end. My language knowledge reflects that, with a strong grasp on PHP, Javascript (and several packages like VueJS, D3, and JQuery), and CSS/SCSS. I have experience with several CMS including Concrete5, Wordpress, Drupal, and Wix (and a resulting passion for finding the cleanest way to implement each CMS).</p> -->
+                            <!-- <p>A little more personally, my three favourite things are wine, sleeping, and my cats. An average night consists of me playing Dragon Age: Inquisition while I learn random trivia facts on the internet (because the internet never lies). I aspire to be the most basic I can be.</p> -->
+                        </div>
+                    </div>
                 </div>
-                <div class="copy">
-                    <p>I’m Tina, a full-stack developer with a passion for all things front-end. My language knowledge reflects that, with a strong grasp on PHP, Javascript (and several packages like VueJS, D3, and JQuery), and CSS/SCSS. I have experience with several CMS including Concrete5, Wordpress, Drupal, and Wix (and a resulting passion for finding the cleanest way to implement each CMS).</p>
-
-                    <p>A little more personally, my three favourite things are wine, sleeping, and my cats. An average night consists of me playing Dragon Age: Inquisition while I learn random trivia facts on the internet (because the internet never lies). I aspire to be the most basic I can be.</p>
+                <div class="row">
+                    <div class="col-md-6">
+                        <h1 class="card-title">Tools/Languages</h1>
+                        <div class="card">
+                            - list of important languages/tools I know/use
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <h1 class="card-title">Fun Facts</h1>
+                        <div class="card">
+                            - list of random facts
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card"></div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="card-title">Skills</h1>
+                        <div class="card">
+                            - this is where a graph of my skills will go. 
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
 
-        <section class="skillsSection" id="skillsSection">
-            <div class="wrapper">
-                <h2><span class="number text-secondary">02/</span> Skills.</h2>
-                <div class="socialMedia">
-                    <a href="https://www.linkedin.com/in/tinavv" target="_blank">
-                        <img src="/images/social_media/linkedin-logo.png" alt="LinkedIn" />
-                    </a>
-                    <a href="https://github.com/tinavandervecht" target="_blank">
-                        <img src="/images/social_media/github-logo.png" alt="GitHub" />
-                    </a>
+        <section id="contactSection">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h1 class="card-title">Contact</h1>
+                    </div>
                 </div>
-
-                <?php include("./partials/skills.php"); ?>
-
-            </div>
-        </section>
-
-        <section class="bg-white contactSection" id="contactSection">
-            <div class="wrapper">
-                <h2><span class="number text-secondary">03/</span> Contact.</h2>
-                <div class="left-column">
-                    <p>Get in touch and say hello!<br/>
-                    Whether you have a work enquiry or<br/>
-                    simply want to find out more, let’s talk.</p>
-                    <a href="mailto:tvandervecht@gmail.com">Send me an email.</a>
-                </div>
-                <div class="right-column">
-                    <form method="POST" action="./form.php">
-                        <div class="input  <?php echo getEmptyError('first_name', 'empty'); ?>" id="first-name">
-                            <label for="first_name">First Name</label>
-                            <input type="text" name="first_name" value="<?php echo getValue('first_name'); ?>" />
-                            <small class="error-empty-message">First name is required.</small>
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="card">
+                            <p>Get in touch and say hello!<br/>
+                            Whether you have a work enquiry or<br/>
+                            simply want to find out more, let’s talk.</p>
+                            <a href="mailto:tvandervecht@gmail.com">Send me an email.</a>
                         </div>
-                        <div class="input <?php echo getEmptyError('last_name', 'empty'); ?>" id="last-name">
-                            <label for="last_name">Last Name</label>
-                            <input type="text" name="last_name" value="<?php echo getValue('last_name'); ?>" />
-                            <small class="error-empty-message">Last name is required.</small>
+                    </div>
+                    <div class="col-md-9">
+                        <div class="card">
+                            <form method="POST" action="./form.php">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <div class="input  <?php echo getEmptyError('first_name', 'empty'); ?>" id="first-name">
+                                            <label for="first_name">First Name</label>
+                                            <input type="text" name="first_name" value="<?php echo getValue('first_name'); ?>" />
+                                            <small class="error-empty-message">First name is required.</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="input <?php echo getEmptyError('last_name', 'empty'); ?>" id="last-name">
+                                            <label for="last_name">Last Name</label>
+                                            <input type="text" name="last_name" value="<?php echo getValue('last_name'); ?>" />
+                                            <small class="error-empty-message">Last name is required.</small>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="input <?php echo getEmptyError('email', 'empty'); ?> <?php echo getEmptyError('email', 'invalid'); ?>" id="email">
+                                            <label for="email">Email</label>
+                                            <input type="text" name="email" value="<?php echo getValue('email'); ?>" />
+                                            <small class="error-empty-message">Email is required.</small>
+                                            <small class="error-invalid-message">Email must be valid.</small>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="input" id="website">
+                                    <label for="website">Website</label>
+                                    <input type="text" name="website" />
+                                </div>
+                                <div class="input <?php echo getEmptyError('message', 'empty');?>" id="message">
+                                    <label for="message">Message</label>
+                                    <textarea name="message" rows="5"><?php echo getValue('message'); ?></textarea>
+                                    <small class="error-empty-message">A message is required.</small>
+                                </div>
+                                <button type="submit">Send Message</button>
+                            </form>
                         </div>
-                        <div class="input <?php echo getEmptyError('email', 'empty'); ?> <?php echo getEmptyError('email', 'invalid'); ?>" id="email">
-                            <label for="email">Email</label>
-                            <input type="text" name="email" value="<?php echo getValue('email'); ?>" />
-                            <small class="error-empty-message">Email is required.</small>
-                            <small class="error-invalid-message">Email must be valid.</small>
-                        </div>
-                        <div class="input" id="website">
-                            <label for="website">Website</label>
-                            <input type="text" name="website" />
-                        </div>
-                        <div class="input <?php echo getEmptyError('message', 'empty');?>" id="message">
-                            <label for="message">Message</label>
-                            <textarea name="message" rows="5"><?php echo getValue('message'); ?></textarea>
-                            <small class="error-empty-message">A message is required.</small>
-                        </div>
-                        <button type="submit">Send Message</button>
-                    </form>
+                    </div>
                 </div>
             </div>
         </section>
