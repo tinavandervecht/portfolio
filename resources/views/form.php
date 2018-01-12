cde5c617eaa40628605733852eda3dfdb42c99f2
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-01-12 18:12:40 -0500

    Getting emails to send on go daddy server holy shit this is awful

--- resources/views/form.php
+++ resources/views/form.php
@@ -1,5 +1,12 @@
 <?php
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/phpmailer/phpmailer/src/Exception.php';
+require '../vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require '../vendor/phpmailer/phpmailer/src/SMTP.php';
+
 $errors = array();
 $values = array();
 
@@ -52,21 +59,41 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         exit;
     }
 
+    try {
+        $mail = new PHPMailer(true);
+        $mail->Host       = "mail.tinavv.com";
+    	$mail->Port       = 143;
+    	$mail->SMTPDebug  = 0;
+    	$mail->SMTPSecure = "none";
+    	$mail->SMTPAuth   = false;
+    	$mail->Username   = "_mainaccount@tinavv.com";
+       	$mail->Password = "winter!Soldier97";
+
+        $mail->setFrom('_mainaccount@tinavv.com');
+        $mail->addAddress('tvandervecht@gmail.com');
+
+        $mail->isHTML(true);
+        $mail->Subject = "Portfolio Form Submission From " . $values['first_name'] . ' ' . $values['last_name'];
+        $mail->Body    = "<h1>This is a portfolio submission.</h1>
+            <h3>Name: " . $values['first_name'] . " " . $values['last_name'] . "</h3>\n
+            <h3>Email: " . $values['email'] . "</h3>\n
+            <h3>Message: " . $values['message'] . "</h3>";
+        $mail->AltBody = "Name: " . $values['first_name'] . " " . $values['last_name'] . " \n
+        Email: " . $values['email'] . "\n
+        Message: " . $values['message'];
+
+        $mail->send();
+        $values['sent'] = true;
+    } catch (Exception $e) {
+        $values['sent'] = false;
+        $values['reason'] = $mail->ErrorInfo;
+    }
+
     $emails = file_get_contents('./emails.json');
     $emails = json_decode($emails, true);
     $emails['email-' . count($emails)] = $values;
 
     file_put_contents('./emails.json', json_encode($emails));
-
-    $to = "tvandervecht@gmail.com";
-    $subject = "Portfolio Form Submission From " . $values['first_name'] . ' ' . $values['last_name'];
-    $msg = "This is a portfolio submission.\n\n
-    name: " . $values['first_name'] . " " . $values['last_name'] . "\n
-    email: " . $values['email'] . "\n
-    message: " . $values['message'];
-    $headers = "From: tvandervecht+portfolio_submission@gmail.com" . "\r\n";
-
-    mail($to,$subject,$msg,$headers);
 
     $_SESSION['success'] = true;
     header('Location: ' . $_SERVER['HTTP_REFERER']);
