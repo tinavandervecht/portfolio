1b02eddc6eddd6e07e198fe4e6c5e325a0b5ae1a
Tina Vandervecht <tina.vandervecht@roomroster.com>
2018-03-09 16:22:40 -0500

    building out basic index page

--- public/index.php
+++ public/index.php
@@ -1,4 +1,4 @@
-<!doctype html>
+<!DOCTYPE HTML>
 
 <html lang="en">
 <head>
@@ -10,16 +10,12 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="google-site-verification" content="Usp5nBFgOCUn7dJnFJjqeG-7_9SuBPuc1qB2lR2bJSU" />
 
-    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,400|VT323:100,400" rel="stylesheet">
-    <link rel="stylesheet" href="/css/app.css" async>
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]-->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/css/app.css" />
 
 </head>
 
-<body>
+<body class="is-loading">
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112380740-1"></script>
     <script>
@@ -30,13 +26,51 @@
       gtag('config', 'UA-112380740-1');
     </script>
 
-    <div id="main">
+    <div id="wrapper">
 
-        <footer class="footer">
+        <header id="header">
+			<div class="logo">
+				<span class="fa fa-diamond"></span>
+			</div>
+			<div class="content">
+				<div class="inner">
+					<h1>Tina Vandervecht</h1>
+					<p>Developer. Passionate about trivia. Chronic talker.</p>
+				</div>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="#about">About</a></li>
+					<li><a href="#contact">Contact</a></li>
+				</ul>
+			</nav>
+		</header>
+
+        <section id="main">
+            <article id="about">
+				<h2 class="major">About</h2>
+			</article>
+
+            <article id="contact">
+				<h2 class="major">Contact</h2>
+				<ul class="icons">
+					<li><a href="#" class="fa fa-twitter"><span class="sr-only">Twitter</span></a></li>
+					<li><a href="#" class="fa fa-instagram"><span class="sr-only">Instagram</span></a></li>
+                    <li><a href="#" class="fa fa-linkedin"><span class="sr-only">LinkedIn</span></a></li>
+					<li><a href="#" class="fa fa-github"><span class="sr-only">GitHub</span></a></li>
+				</ul>
+			</article>
+        </section>
+
+        <footer id="footer">
             <p class="text-secondary">Copyright &copy; <?php echo date('Y'); ?> Tina Vandervecht.</p>
         </footer>
 
     </div>
-    <script src="js/app.js" async></script>
+    <div id="bg" data-img-key="<?php echo rand(1, 7) ?>">
+        <span class="after"></span>
+    </div>
+
+    <script src="/js/app.js"></script>
 </body>
 </html>
